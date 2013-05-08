038ef1d2647f1c3974b6f73d5bfabe933a73c614
Stephen Bussey <sb8244@ubuntu.(none)>
2013-05-07 23:43:09 -0700

    Change goal query

--- goaltreeselects.sql
+++ goaltreeselects.sql
@@ -12,7 +12,7 @@ idea is to just delete all subnodes
 
 SELECT * FROM 
 (
-	SELECT DISTINCT User_Goal.user_id, Goal.goal_id, title, due_date, NULL as parent_id, NULL as child_id, root FROM User_Goal 
+	SELECT DISTINCT User_Goal.user_id, Goal.goal_id, title, due_date, NULL as parent_id, NULL as child_id, root, completed_timestamp FROM User_Goal 
 		JOIN Goal ON User_Goal.goal_id = Goal.goal_id
 		LEFT OUTER JOIN GoalTreeChildren ON Goal.goal_id = GoalTreeChildren.goal_id OR Goal.goal_id = GoalTreeChildren.child_id
 		JOIN Task ON Goal.task_id = Task.task_id
@@ -20,7 +20,7 @@ SELECT * FROM
 
 	UNION
 
-	SELECT DISTINCT User_Goal.user_id, GoalTreeChildren.goal_id as goal_id, title, due_date, null as parent_id, GoalTreeChildren.child_id as child_id, root FROM User_Goal
+	SELECT DISTINCT User_Goal.user_id, GoalTreeChildren.goal_id as goal_id, title, due_date, null as parent_id, GoalTreeChildren.child_id as child_id, root, completed_timestamp FROM User_Goal
 		JOIN GoalTreeChildren ON User_Goal.goal_id = GoalTreeChildren.child_id
 		JOIN Goal ON GoalTreeChildren.goal_id = Goal.goal_id
 		JOIN Task ON Goal.task_id = Task.task_id 
@@ -28,7 +28,7 @@ SELECT * FROM
 
 	UNION
 
-	SELECT DISTINCT User_Goal.user_id, GoalTreeChildren.child_id as goal_id, title, due_date, GoalTreeChildren.goal_id as parent_id, null as child_id, root FROM User_Goal
+	SELECT DISTINCT User_Goal.user_id, GoalTreeChildren.child_id as goal_id, title, due_date, GoalTreeChildren.goal_id as parent_id, null as child_id, root, completed_timestamp FROM User_Goal
 		JOIN GoalTreeChildren ON User_Goal.goal_id = GoalTreeChildren.goal_id
 		JOIN Goal ON GoalTreeChildren.goal_id = Goal.goal_id
 		JOIN Task ON Goal.task_id = Task.task_id 
--- personagoal/models/goaltree.js
+++ personagoal/models/goaltree.js
@@ -1,5 +1,5 @@
 var mysql = require('./mysql');
-var selectStatements = 'SELECT * FROM ( SELECT DISTINCT User_Goal.user_id, Goal.goal_id, title, due_date, NULL as parent_id, NULL as child_id, root FROM User_Goal JOIN Goal ON User_Goal.goal_id = Goal.goal_id LEFT OUTER JOIN GoalTreeChildren ON Goal.goal_id = GoalTreeChildren.goal_id OR Goal.goal_id = GoalTreeChildren.child_id JOIN Task ON Goal.task_id = Task.task_id WHERE User_Goal.user_id = ? UNION SELECT DISTINCT User_Goal.user_id, GoalTreeChildren.goal_id as goal_id, title, due_date, null as parent_id, GoalTreeChildren.child_id as child_id, root FROM User_Goal JOIN GoalTreeChildren ON User_Goal.goal_id = GoalTreeChildren.child_id JOIN Goal ON GoalTreeChildren.goal_id = Goal.goal_id JOIN Task ON Goal.task_id = Task.task_id WHERE User_Goal.user_id = ? UNION SELECT DISTINCT User_Goal.user_id, GoalTreeChildren.child_id as goal_id, title, due_date, GoalTreeChildren.goal_id as parent_id, null as child_id, root FROM User_Goal JOIN GoalTreeChildren ON User_Goal.goal_id = GoalTreeChildren.goal_id JOIN Goal ON GoalTreeChildren.goal_id = Goal.goal_id JOIN Task ON Goal.task_id = Task.task_id WHERE User_Goal.user_id = ? ) a ORDER BY goal_id';
+var selectStatements = 'SELECT * FROM ( SELECT DISTINCT User_Goal.user_id, Goal.goal_id, title, due_date, NULL as parent_id, NULL as child_id, root, completed_timestamp FROM User_Goal JOIN Goal ON User_Goal.goal_id = Goal.goal_id LEFT OUTER JOIN GoalTreeChildren ON Goal.goal_id = GoalTreeChildren.goal_id OR Goal.goal_id = GoalTreeChildren.child_id JOIN Task ON Goal.task_id = Task.task_id WHERE User_Goal.user_id = ? UNION SELECT DISTINCT User_Goal.user_id, GoalTreeChildren.goal_id as goal_id, title, due_date, null as parent_id, GoalTreeChildren.child_id as child_id, root, completed_timestamp FROM User_Goal JOIN GoalTreeChildren ON User_Goal.goal_id = GoalTreeChildren.child_id JOIN Goal ON GoalTreeChildren.goal_id = Goal.goal_id JOIN Task ON Goal.task_id = Task.task_id WHERE User_Goal.user_id = ? UNION SELECT DISTINCT User_Goal.user_id, GoalTreeChildren.child_id as goal_id, title, due_date, GoalTreeChildren.goal_id as parent_id, null as child_id, root, completed_timestamp FROM User_Goal JOIN GoalTreeChildren ON User_Goal.goal_id = GoalTreeChildren.goal_id JOIN Goal ON GoalTreeChildren.goal_id = Goal.goal_id JOIN Task ON Goal.task_id = Task.task_id WHERE User_Goal.user_id = ? ) a ORDER BY goal_id';
 
 exports.getTreeForUser = function(user_id, callback) {
 	params = [ user_id, user_id, user_id ];
@@ -26,6 +26,7 @@ var constructGoalTree = function(results, callback) {
 		nodes[item.goal_id].children = {};
 		nodes[item.goal_id].parent = null;
 		nodes[item.goal_id].root = item.root;
+		nodes[item.goal_id].completed = item.completed_timestamp;
 	});
 	results.forEach(function(item) {
 		if(nodes[item.goal_id].data == undefined) {
